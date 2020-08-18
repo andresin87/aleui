83f03e105367b4a46e41be5ba084c220bfe57351
“andresin87” <andres.lucas@adevinta.com>
2020-08-18 10:45:51 +0200

    Mod ui difference

--- packages/button/__stories__/index.stories.js
+++ packages/button/__stories__/index.stories.js
@@ -8,8 +8,8 @@ export default {
 export const basicButton = () => <Button> Basic Button</Button>;
 export const coloredButton = () => (
   <>
-    <Button type="primary"> Primary Button</Button>
-    <Button type="secondary"> Secondary Button</Button>
+    <Button type="primary">Primary Button</Button>
+    <Button type="secondary">Secondary Button</Button>
   </>
 );
 export const disabledButton = () => <Button disabled> Primary Button</Button>;
